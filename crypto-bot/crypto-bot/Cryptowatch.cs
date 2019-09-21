ead5a727fdccb12c99a5589e21bbc578b2f51e6a
yikaip <ypeng@uwyo.edu>
2019-09-21 16:55:37 -0600

    Added Initial Cryptowatch Class

---
+++ crypto-bot/crypto-bot/Cryptowatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace crypto_bot
+{
+    public class Cryptowatch
+    {
+        private string assets;
+        private string pairs;
+        private string exchanges;
+        private string markets;
+        private Uri BaseAddress;
+        private readonly HttpClient client = new HttpClient();
+
+        public void Crytowatch() {
+            {
+                BaseAddress = new Uri("https://api.cryptowat.ch");
+            }
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public void getAssetss()
+        {
+
+        }
+        public void getPairs()
+        {
+
+        }
+        public void getExchanges()
+        {
+
+        }
+        public void getMarkets()
+        {
+
+        }
+    }
+}
+      
