014f327492a9fe9cb9aed1fddce5eb1bcb8125cf
Winston Howard <winston.howard@yahoo.com>
2019-09-21 21:21:17 -0600

    Implimented Offer Functionality

--- crypto-bot/crypto-bot/Commands.cs
+++ crypto-bot/crypto-bot/Commands.cs
@@ -18,7 +18,7 @@ namespace crypto_bot
                 Console.WriteLine("**LOGFILE: USER: " + Context.User.Username + ", <SHOW EXCHANGES MODULE>");
                 var user = Context.User as SocketGuildUser;                
                 Cryptowatch cryptowatch = new Cryptowatch();
-                var res = await cryptowatch.getExchanges();
+                var res = await cryptowatch.GetExchanges();
                 if (res.Count > 0)
                 {
                     res.Sort();
@@ -49,7 +49,7 @@ namespace crypto_bot
                 Console.WriteLine("**LOGFILE: USER: " + Context.User.Username + ", <SHOW MARKETS MODULE>");
                 var user = Context.User as SocketGuildUser;
                 Cryptowatch cryptowatch = new Cryptowatch();
-                var res = await cryptowatch.getMarket(paramInput);
+                var res = await cryptowatch.GetMarket(paramInput);
                 if (res.Count > 0)
                 {
                     res.Sort();
@@ -64,6 +64,40 @@ namespace crypto_bot
                 else
                 {
                     await Context.Channel.SendMessageAsync("I couldn't find any market information about that exchange, please recheck the exchange name you gave me.");
+                }
+            }
+            catch (Exception a)
+            {
+                Console.Write("+++SHOW MARKETS ERROR: CRASH: " + a);
+            }
+        }
+
+        [Command("o")]
+        public async Task SHOW_OFFER([Remainder] string paramInput = "0")
+        {
+            try
+            {
+                Console.WriteLine("**LOGFILE: USER: " + Context.User.Username + ", <SHOW OFFER MODULE>");
+                var user = Context.User as SocketGuildUser;
+                Cryptowatch cryptowatch = new Cryptowatch();
+                var @params = paramInput.Split(' ');
+                string market = @params[0]; string pair = @params[1];
+                var res = await cryptowatch.GetOffer(market,pair);
+                if (res.Count > 5)
+                {
+                    /*Result Structure
+                Exchange(0) | pair(1)
+                    "Offer Volume:" volume(2) | "% Change:" change(3)
+                            | "Last Price:" last(4) 
+                            | "Max Price:" max(5)
+                            | "Min Price:" min(6)
+                 */
+                    await Context.Channel.SendMessageAsync("__Detailed Offer Information For "+pair.ToUpper()+" Currency Exchanges From The "+market+" Exchange__");
+                    await Context.Channel.SendMessageAsync("```"+res[0]+" | "+res[1]+"\n\t"+"Volume: "+ Math.Round((double)res[2],2)+" | "+"Change:"+Math.Round((double)res[3], 2)+"%\n\t | "+"Last Price:"+res[4]+"\n\t | "+"High Price: "+res[5]+ "\n\t | " + "Low Price: " + res[6]+"```");
+                }
+                else
+                {
+                    await Context.Channel.SendMessageAsync("I couldn't find detailed offer information for that offer and exchange, please recheck the exchange name and offer pair you gave me.");
                 }
             }
             catch (Exception a)
@@ -95,7 +129,7 @@ namespace crypto_bot
             {
                 Console.WriteLine("**LOGFILE: USER: " + Context.User.Username + ", <SHOW CW MODULE>");
                 var user = Context.User as SocketGuildUser;
-                await Context.Channel.SendMessageAsync("Cryptowatch is a platform for the cryptocurrency markets. Our mission is to provide one powerful interface to scan prices, analyze market movements, and make trades on every major exchange. For more information, please visit https://cryptowat.ch/about");
+                await Context.Channel.SendMessageAsync("Cryptowatch is a platform for the cryptocurrency markets. Our mission is to provide one powerful interface to scan prices, analyze market movements, and make trades on every major exchange. For more information, please visit https://cryptowat.ch/about",true);
                 //cryptowatch.getExchanges();
             }
             catch (Exception a)
